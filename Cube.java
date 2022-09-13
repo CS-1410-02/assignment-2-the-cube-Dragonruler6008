bd9d9fe639669a3ee3f8e18a410601b8f51e14cf
Dragonruler6008 <dragonruler6008@gmail.com>
2022-09-13 00:30:59 -0600

    Moving left and right works. Co-authored-by: thesavagen <thesavagen@users.noreply.github.com>

--- Cube.java
+++ Cube.java
@@ -7,19 +7,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Cube {
+      //Create the cubemap
+      static String[][] cubemap = new String[9][12];
+      //Storage for the cubemap
+      static String[][] cubemapPrevious = new String[9][12];
 	public static void main(final String[] args)
   throws IOException
   {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); // for later input
+    boolean argsCheck = false;
 
+    boolean proceed = true;
+    int argsRunIndex = 0;
+    if(args.length > 0){
+            argsCheck = true;
+        }
     //Create the color map
     String[][][] temp = new String[6][3][3];
     //Set colors to a color array
     String[] colors = new String[] {"w","r","b","o","g","y"};
-    //Create the cubemap
-    String[][] cubemap = new String[9][15];
-    //Storage for the cubemap
-    String[][] cubemapPrevious = new String[9][15];
+
     
     //Setting all colors properly on the rubics cube
     // I = center color of rubics cube
@@ -37,8 +44,8 @@ public class Cube {
 
         //top 3 spaces of x axis
       for(int j=0;j<3;j++){
-        for(int k=8;k<11;k++) {
-          cubemap[j][k] = (temp[0][k-8][j]);
+        for(int k=6;k<9;k++) {
+          cubemap[j][k] = (temp[0][k-6][j]);
         }
       }
 
@@ -47,41 +54,62 @@ public class Cube {
         for(int k=0;k<3;k++){
           cubemap[j][k] = temp[1][k][j-3];
         }
-        for(int k=4;k<7;k++){
-          cubemap[j][k] = temp[2][k-4][j-3];
+        for(int k=3;k<6;k++){
+          cubemap[j][k] = temp[2][k-3][j-3];
         }
-        for(int k=8;k<11;k++){
-          cubemap[j][k] = temp[3][k-8][j-3];
+        for(int k=6;k<9;k++){
+          cubemap[j][k] = temp[3][k-6][j-3];
         }
-       for(int k=12;k<15;k++){
-          cubemap[j][k] = temp[4][k-12][j-3];
+       for(int k=9;k<12;k++){
+          cubemap[j][k] = temp[4][k-9][j-3];
         }
       }
 
         //bottom 3 spaces of x axis
       for(int j=6;j<9;j++){
-        for(int k=8;k<11;k++) {
-          cubemap[j][k] = (temp[5][k-8][j-6]);
+        for(int k=6;k<9;k++) {
+          cubemap[j][k] = (temp[5][k-6][j-6]);
         }
       }
       for(int j=0; j<9; j++){
-                for(int k=0; k<15; k++){
+                for(int k=0; k<12; k++){
                     if(cubemap[j][k] == null)
                        cubemap[j][k] = "_";
       }
       }
     for(int j=0;j<9;j++) {
-      for(int k=0;k<15;k++) {
+      for(int k=0;k<12;k++) {
         cubemapPrevious[j][k] = cubemap[j][k];
       }
     }
     for(int j=0;j<9;j++) {
-      for(int k=0;k<15;k++) {
+      for(int k=0;k<12;k++) {
         System.out.printf(cubemapPrevious[j][k]);
       }
       System.out.println();
     }
 
+    //moving j=4 by 3 to the right
+    //works for 
+    // for (int k=0;k<12;k++) {
+    //   if(k>=3) {
+    //     cubemap[4][k] = cubemapPrevious[4][k-3];
+    //   } else {
+    //     cubemap[4][k] = cubemapPrevious[4][k+9];
+    //   }
+    //   System.out.printf(cubemap[4][k]);
+    // }
+
+    //moving j=4 by 3 to the left
+    // Works for U and D'
+    // for (int k=0;k<12;k++) {
+    //   if(k>=9) {
+    //     cubemap[4][k] = cubemapPrevious[4][k-9];
+    //   } else {
+    //     cubemap[4][k] = cubemapPrevious[4][k+3];
+    //   }
+    //   System.out.printf(cubemap[4][k]);
+    // }
 
 
 
@@ -103,6 +131,77 @@ public class Cube {
             
             
 
+      //stolen
+        while(proceed){   
+          String input;
+
+             if(!argsCheck){
+                input = reader.readLine();
+            }else{
+                if(argsRunIndex == args.length){
+                    argsCheck=false;
+                    input = "e";
+                }else{
+                    input = args[argsRunIndex];
+                    argsRunIndex++;
+                }
+            }
+            
+            switch(input){
+              //Top of cube left
+                case "U":
+                    left(3);
+                    displayCubeMap();
+                    break;
+              //Top Bottom of Cube Right
+                case "D":
+                    right(5);
+                    displayCubeMap();
+                    break;
+              //Right side of cube up
+                case "R":
+                    break;
+              //Left side of cube down
+                case "L":
+                    break;
+              //Front face clockwise
+                case "F":
+                    break;
+              //Back face counterclockwise
+                case "B":
+                    break;
+              //Top of cube right
+                case "U'":
+                    right(3);
+                    displayCubeMap();
+                    break;
+              //Bottom of cube left
+                case "D'":
+                    left(5);
+                    displayCubeMap();
+                    break;
+              //Right side of cube down
+                case "R'":
+                    break;
+              //Left side of cube up
+                case "L'":
+                    break;
+              //Front face counterclockwise
+                case "F'":
+                    break;
+              //Back face of cube clockwise
+                case "B'":
+                    break;
+              //Stop solving / give up
+                case "q":
+                    proceed = false;
+                    System.out.println("QUITTER!");
+                    break;
+                default:
+                    System.out.println("You must enter one of the commands!");
+            }
+            }
+
       //THIS IS CHECKING ALL OF THE LAYER COLORS IN A MORE DIFFICULT WAY TO READ IT. Maybe useful later
       // for(int i=0; i<6; i++) {
       // for( int j=0; j<3; j++){
@@ -112,5 +211,33 @@ public class Cube {
       //     System.out.println();
       //   }
       // }
+  }
+  private static void displayCubeMap() {
+    for(int j=0;j<9;j++) {
+      for(int k=0;k<12;k++){
+        System.out.printf(cubemap[j][k]);
+        cubemapPrevious[j][k] = cubemap[j][k];
+      }
+      System.out.println();
+    }
+  }
+  private static void left(int l){
+    for (int k=0;k<12;k++) {
+      if(k>=9) {
+        cubemap[l][k] = cubemapPrevious[l][k-9];
+      } else {
+        cubemap[l][k] = cubemapPrevious[l][k+3];
+      }
+    }
+  }
+  private static void right(int l){
+    for (int k=0;k<12;k++) {
+      if(k>=3) {
+        cubemap[l][k] = cubemapPrevious[l][k-3];
+      } else {
+        cubemap[l][k] = cubemapPrevious[l][k+9];
+      }
+
+    }
   }
 }
